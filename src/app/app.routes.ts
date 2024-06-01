f0ea87c29e69a33dabf01e9392db38af2ef21029
arodoo <haro.em@hotmail.com>
2024-06-01 08:50:30 -0600

    refactor: Update app component structure and styles for better organization and consistency

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home/home.component';
+import path from 'node:path';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+    path: '',
+    component: HomeComponent
+}
+];
